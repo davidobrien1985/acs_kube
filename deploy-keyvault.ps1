f5bad88906a67f757ea1758f089f7ccee26676ce
David OBrien <me@david-obrien.net>
2017-10-20 12:52:58 +1100

    Remove obsolete files.

--- deploy-keyvault.ps1
+++
@@ -1,8 +0,0 @@
-$rgName = "acs-kube-keyvault"
-$location = "australiasoutheast"
-$tags = @{Name="acs-kube"}
-
-
-Login-AzureRmAccount
-New-AzureRmResourceGroup -Name $rgName -Location $location -Verbose -Tag $tags
-New-AzureRmResourceGroupDeployment -Name $rgName -ResourceGroupName $rgName -Mode Incremental -DeploymentDebugLogLevel All -TemplateFile .\armdeploy-keyvault.json -Verbose
--- deploy.ps1
+++
@@ -1,7 +0,0 @@
-#Login-AzureRmAccount
-
-$rgName = 'doacskube'
-
-New-AzureRmResourceGroup -Name $rgName -Location australiasoutheast -Verbose -Force
-
-New-AzureRmResourceGroupDeployment -ResourceGroupName $rgName -TemplateFile .\armdeploy.json -TemplateParameterFile .\armdeploy-params.json -Mode Complete -Verbose -Force
